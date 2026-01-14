3aed88ce99e5c52cdecc998311054618e2ec3098
Abhijit5463 <70604463@users.noreply.github.com>
2026-01-14 12:36:38 +0530

    resolving deloyment issue

--- src/main/java/com/example/issueflow/controller/SpaController.java
+++ src/main/java/com/example/issueflow/controller/SpaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SpaController {
 
-    @GetMapping({ "/", "/{path:[^\\.]*}", "/{path:^(?!api|v3|swagger-ui|error).*$}/**" })
+    @GetMapping({ "/", "/{path:[^\\.]*}", "/{path:^(?!api|v3|swagger-ui|error).*$}/**/{subpath:[^\\.]*}" })
     public String forward() {
         return "forward:/index.html";
     }
